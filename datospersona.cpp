7dbc52ff3f4edbd460af232806a6277aeeb33776
matkomilovic666 <37634969+matkomilovic666@users.noreply.github.com>
2021-02-21 09:55:22 -0300

    Add files via upload

---
+++ datospersona.cpp
@@ -0,0 +1,26 @@
+//Primer programa po
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	
+	int edad;
+	char sexo[10];
+	float altura;
+	
+	cout<<"Ingrese su edad ";
+	cin>>edad;
+	cout<<"Ingrese su sexo: ";
+	cin>>sexo;
+	cout<<"Ingrese su altura en metros: ";
+	cin>>altura;
+
+	cout<<"\nEdad: "<<edad<<endl;
+	cout<<"\nSexo: "<<sexo<<endl;
+	cout<<"\nAltura: "<<altura<<endl;
+
+	return 0;
+}
